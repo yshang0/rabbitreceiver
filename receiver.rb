5743f7ab7d35b1674d9e72794dd4d6b77aacff1e
yshang0 <shanglue101@gmail.com>
2019-04-17 20:11:18 -0400

    fix

--- receiver.rb
+++ receiver.rb
@@ -1,26 +1,38 @@
+
+require 'sinatra'
 require 'bunny'
 
-connection = Bunny.new ENV['CLOUDAMQP_URL']
+# connection = Bunny.new ENV['CLOUDAMQP_URL']
 
-connection.start
-channel = connection.create_channel
-queue = channel.queue('hello')
+connection = Bunny.new(host:  'localhost',
+                  port:  '5672',
+                  vhost: '/',
+                  user:  'guest',
+                  pass:  'guest')
 
-begin
+
+get '/' do
+
+	connection.start
+	channel = connection.create_channel
+	queue = channel.queue('hello')
+
+	# begin
 	puts ' [*] Waiting for messages. To exit press CTRL+C'
 	queue.subscribe(block: true) do |_delivery_info, _properties, body|
 		puts " [x] Received #{body}"
+		@information=body
 	end
 
-rescue Interrupt => _
+	# end
+	# rescue Interrupt => _
+	# 	connection.close
+	# 	exit(0)
+	# end
+
 	connection.close
 
-	exit(0)
+	# redirect '/'
+
 end
 
-
-# (host:  'localhost',
-#                   port:  '5672',
-#                   vhost: '/',
-#                   user:  'guest',
-#                   pass:  'guest')
